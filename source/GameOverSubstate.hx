b099ae1f271b93d83d5acbd2e89b66a865601a9b
HeartPink <89818466+HeartPink@users.noreply.github.com>
2022-01-09 18:37:35 -0600

    look ma, funkin

---
+++ source/GameOverSubstate.hx
@@ -0,0 +1,244 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSubState;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.group.FlxGroup;
+import openfl.Assets;
+import sys.FileSystem;
+
+
+
+class GameOverSubstate extends MusicBeatSubstate
+{
+	public var boyfriend:Boyfriend;
+	var camFollow:FlxPoint;
+	var camFollowPos:FlxObject;
+	var updateCamera:Bool = false;
+
+	var curWacky:FlxText;
+	var credGroup:FlxGroup;
+	var money:FlxText;
+	
+
+	var stageSuffix:String = "";
+
+	public static var characterName:String = 'bf';
+	public static var deathSoundName:String = 'fnf_loss_sfx';
+	public static var loopSoundName:String = 'gameOver';
+	public static var endSoundName:String = 'gameOverEnd';
+
+	public static var instance:GameOverSubstate;
+
+	public static function resetVariables() {
+		characterName = 'bf';
+		deathSoundName = 'fnf_loss_sfx';
+		loopSoundName = 'gameOver';
+		endSoundName = 'gameOverEnd';
+	}
+
+	override function create()
+	{
+		instance = this;
+		PlayState.instance.callOnLuas('onGameOverStart', []);
+
+		super.create();
+	}
+
+	public function new(x:Float, y:Float, camX:Float, camY:Float)
+	{
+		super();
+
+		PlayState.instance.setOnLuas('inGameOver', true);
+
+		Conductor.songPosition = 0;
+
+		boyfriend = new Boyfriend(x, y, characterName);
+		boyfriend.x += boyfriend.positionArray[0];
+		boyfriend.y += boyfriend.positionArray[1];
+		add(boyfriend);
+
+		camFollow = new FlxPoint(boyfriend.getGraphicMidpoint().x, boyfriend.getGraphicMidpoint().y);
+
+		FlxG.sound.play(Paths.sound(deathSoundName));
+		Conductor.changeBPM(100);
+		// FlxG.camera.followLerp = 1;
+		// FlxG.camera.focusOn(FlxPoint.get(FlxG.width / 2, FlxG.height / 2));
+		FlxG.camera.scroll.set();
+		FlxG.camera.target = null;
+
+		boyfriend.playAnim('firstDeath');
+
+		var exclude:Array<Int> = [];
+
+		camFollowPos = new FlxObject(0, 0, 1, 1);
+		camFollowPos.setPosition(FlxG.camera.scroll.x + (FlxG.camera.width / 2), FlxG.camera.scroll.y + (FlxG.camera.height / 2));
+		add(camFollowPos);
+
+
+
+
+				
+
+
+
+
+
+		// DEATH MESSAGE
+
+		//txtWeekTitle = new FlxText(FlxG.width * 0.7, 10, 0, "", 32);
+		//txtWeekTitle.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, RIGHT);
+		
+		/*function createCoolText(textArray:Array<String>, ?offset:Float = 0)
+		{
+			for (i in 0...textArray.length)
+			{
+				money = new FlxText(FlxG.width * 0.7, 10, 0, "", 32);
+				money.setFormat("VCR OSD Mono", 20, FlxColor.WHITE);
+				if(credGroup != null) 
+				{
+					credGroup.add(money);
+				}
+			}
+		}
+
+		function getIntroTextShit():Array<Array<String>>
+		{
+			var fullText:String = Assets.getText(Paths.txt("deathText"));
+
+			var swagGoodArray:Array<Array<String>> = [];
+
+			return swagGoodArray;
+		}
+		
+		// trace("Hello world!");
+		curWacky = FlxG.random.getObject(getIntroTextShit());
+		curWacky = new FlxText(FlxG.width * 0.7, 10, 0, "", 32);
+		curWacky.setFormat("VCR OSD Mono", 20, FlxColor.WHITE);
+		
+
+		add(curWacky);
+		trace(curWacky);
+		
+
+		function getIntroTextShit():Array<Array<String>>
+			{
+				var fullText:String = Assets.getText(Paths.txt("deathText"));
+	
+				var swagGoodArray:Array<Array<String>> = [];
+	
+				return swagGoodArray;
+			}
+
+		var rankText:FlxText = new FlxText(0, 10);
+		rankText.text = 'RANK: GREAT';
+		rankText.setFormat(Paths.font("vcr.ttf"), 32);
+		rankText.size = 30;
+		rankText.screenCenter(X);
+		add(rankText);
+		trace(rankText);*/
+
+
+
+
+
+	}
+
+
+	var isFollowingAlready:Bool = false;
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+
+		PlayState.instance.callOnLuas('onUpdate', [elapsed]);
+		if(updateCamera) {
+			var lerpVal:Float = CoolUtil.boundTo(elapsed * 0.6, 0, 1);
+			camFollowPos.setPosition(FlxMath.lerp(camFollowPos.x, camFollow.x, lerpVal), FlxMath.lerp(camFollowPos.y, camFollow.y, lerpVal));
+		}
+
+		if (controls.ACCEPT)
+		{
+			endBullshit();
+		}
+
+		if (controls.BACK)
+		{
+			FlxG.sound.music.stop();
+			PlayState.deathCounter = 0;
+			PlayState.seenCutscene = false;
+
+			if (PlayState.isStoryMode)
+				MusicBeatState.switchState(new StoryMenuState());
+			else
+				MusicBeatState.switchState(new FreeplayState());
+
+			FlxG.sound.playMusic(Paths.music('freakyMenu'));
+			PlayState.instance.callOnLuas('onGameOverConfirm', [false]);
+		}
+
+		if (boyfriend.animation.curAnim.name == 'firstDeath')
+		{
+			if(boyfriend.animation.curAnim.curFrame >= 12 && !isFollowingAlready)
+			{
+				FlxG.camera.follow(camFollowPos, LOCKON, 1);
+				updateCamera = true;
+				isFollowingAlready = true;
+			}
+
+			if (boyfriend.animation.curAnim.finished)
+			{
+				coolStartDeath();
+				boyfriend.startedDeath = true;
+			}
+		}
+
+		if (FlxG.sound.music.playing)
+		{
+			Conductor.songPosition = FlxG.sound.music.time;
+		}
+		
+		PlayState.instance.callOnLuas('onUpdatePost', [elapsed]);
+	}
+
+	override function beatHit()
+	{
+		super.beatHit();
+
+		//FlxG.log.add('beat');
+	}
+
+	var isEnding:Bool = false;
+
+	function coolStartDeath(?volume:Float = 1):Void
+	{
+		FlxG.sound.playMusic(Paths.music(loopSoundName), volume);
+	}
+
+	function endBullshit():Void
+	{
+		if (!isEnding)
+		{
+			isEnding = true;
+			boyfriend.playAnim('deathConfirm', true);
+			FlxG.sound.music.stop();
+			FlxG.sound.play(Paths.music(endSoundName));
+			new FlxTimer().start(0.7, function(tmr:FlxTimer)
+			{
+				FlxG.camera.fade(FlxColor.BLACK, 2, false, function()
+				{
+					MusicBeatState.resetState();
+				});
+			});
+			PlayState.instance.callOnLuas('onGameOverConfirm', [true]);
+		}
+	}
+}
