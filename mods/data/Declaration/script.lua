8f1956dd54cbfca48613f22d1d739f1e70ea8700
HeartPink <89818466+HeartPink@users.noreply.github.com>
2022-02-17 22:53:21 -0600

    misc shit

--- mods/data/Declaration/script.lua
+++ mods/data/Declaration/script.lua
@@ -1,7 +1,7 @@
 function onUpdate()
 
     for i=0,3 do
-        noteTweenAlpha(i+16, i, math.floor(curStep/9000), 0.1)
+        noteTweenAlpha(i+16, i, math.floor(curStep/9000), 0.0001)
     end
     -- Hides rushdead's notes
 
--- mods/scripts/rushweek.lua
+++ mods/scripts/rushweek.lua
@@ -1,8 +1,6 @@
 --gameover line!
 --todo:figure out how to load death lines from da file i guess
 
--- also to do is figure out how to change the default values of the mfn combo shit!! it covers teddys head and heli gf
-
 function onGameOverStart()
 	makeLuaText("gameovertext", "You fucking. died. Bitch", 0, 30, 680);
 	addLuaText("gameovertext");
